cd61f65f69e2d68617c7de28b55e621af31bb9e0
Bryan <bryanfinesw@gmail.com>
2025-12-05 11:35:53 -0500

    Added book page and styling

--- src/modules/books.js
+++ src/modules/books.js
@@ -1,8 +1,35 @@
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { EventHandler } from "./displayController";
+import daisyBook from "../assets/daisys-delightful-day.png";
+import daisyAward from "../assets/bookfest-award.png";
+import comingSoon from "../assets/coming-soon.jpg";
 
 export const Books = (() => {
+   const bookArray = [
+      {
+         image: daisyBook,
+         award: daisyAward,
+         bookTitle: "Daisy's Delightful Day",
+         content: `Daisy's Delightful Day is the story of Palm Sunday told through the eyes of the donkey. Meet Daisy, a sweet, young donkey who lives on a farm but has big dreams of living in the city. It is another beautiful day on the farm when suddenly, her normal routine gets disrupted. Daisy finds herself in the city! Here, she has a unique encounter with someone who will change her life forever.`,
+         awardSpan: "Third Place Winner of the BookFest Awards Fall 2025",
+         buyNow: true,
+         buyNowLink: "https://www.authorhouse.com/en/bookstore/bookdetails/860746-daisys-delightful-day",
+         buyNowText: "Buy Now",
+         hasAward: true,
+      },
+      {
+         image: comingSoon,
+         award: null,
+         bookTitle: "Seeds",
+         content: `Coming Soon - 2026!`,
+         awardSpan: null,
+         buyNow: false,
+         buyNowLink: "https://www.authorhouse.com/en/bookstore/bookdetails/860746-daisys-delightful-day",
+         buyNowText: "Coming Soon",
+         hasAward: false,
+      }
+   ]
 
    const el = {
       content: () => document.getElementById('content'),
@@ -12,8 +39,61 @@ export const Books = (() => {
       el.content().innerHTML = "";
 
       Header.init();
+      el.content().appendChild(page());
       Footer.init();
       EventHandler.bindEvents();
+   }
+
+   const page = () => {
+      const bookSection = document.createElement("section");
+      bookSection.className = "book-section";
+
+      bookArray.map((book) => {
+         const bookContent = document.createElement("div");
+         const bookImageContainer = document.createElement("div");
+         const bookBlurb = document.createElement("div");
+
+         const bookImage = document.createElement("img");
+         bookImage.src = book.image;
+         bookImageContainer.appendChild(bookImage);
+
+         if (book.hasAward) {
+            const awardImage = document.createElement("img");
+            awardImage.id = "award";
+            awardImage.src = book.award;
+            awardImage.alt = "Award";
+            bookImageContainer.appendChild(awardImage);
+         }
+
+         bookContent.className = "book-content";
+         bookImageContainer.className = "book-img";
+         bookBlurb.className = "book-blurb";
+
+         const bookTitle = document.createElement("h1");
+         const bookDescription = document.createElement("p");
+
+         bookTitle.textContent = book.bookTitle;
+         bookDescription.textContent = book.content;
+
+         if (book.awardSpan) {
+            const awardSpan = document.createElement("span");
+            awardSpan.textContent = book.awardSpan;
+            bookDescription.appendChild(awardSpan);
+         }
+
+         const buyNow = document.createElement("button");
+
+         if (book.buyNow) {
+            buyNow.id = "buyNow"
+         }
+         buyNow.textContent = book.buyNowText;
+
+         bookBlurb.append(bookTitle, bookDescription, buyNow);
+         bookContent.append(bookImageContainer, bookBlurb);
+         bookSection.appendChild(bookContent);
+      });
+
+      return bookSection;
    }
 
    return { init }
